84b3f75486d2f7bd1c96a740e841606d3ad081e2
frolli29 <132070557+frolli29@users.noreply.github.com>
2025-03-31 11:40:11 +0200

    ajout des modifs de laurent
    tentative de copier coller

--- graphic/display.py
+++ graphic/display.py
@@ -3,14 +3,14 @@
 def state2Text(state,type):
     if state==True: 
         if type=='dispenser':
-            text='DISPENSE'
+            text='ON'
         elif type=='circuit entrance':
             text='WATER'
         elif type=='circuit exit':
             text='BIN'
     else:
         if type=='dispenser':
-            text='BOTTLE'
+            text='OFF'
         elif type=='circuit entrance':
             text='IN'
         elif type=='circuit exit':
